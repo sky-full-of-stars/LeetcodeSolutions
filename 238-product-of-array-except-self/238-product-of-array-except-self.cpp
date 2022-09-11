b07cf20d26d22338b0e90c1516d5504aa3a5e5e7
A sky full of stars <akshay.suryanarayan@gmail.com>
2022-09-11 22:08:25 +0530

    Time: 65 ms (5.90%), Space: 24 MB (68.89%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<int> productExceptSelf(vector<int>& nums)
+    vector<int> productExceptSelfBrute(vector<int>& nums)
     {
         int n = nums.size();
         
@@ -11,14 +11,12 @@ public:
             prefixProd[i] = prefixProd[i-1] * nums[i-1];
         }
         
-        
         vector<int> suffixProd(n);
         suffixProd[n-1] = 1;
         for(int i=n-2;i>=0;i--)
         {
             suffixProd[i] = suffixProd[i+1] * nums[i+1];
         }
-        
         
         vector<int> ans;
         for(int i=0;i<n;i++)
@@ -27,4 +25,28 @@ public:
         }
         return ans;
     }   
+    
+    vector<int> productExceptSelf(vector<int>& nums)
+    {
+        int n = nums.size();
+        vector<int> ans(n,1);
+        
+        //let ans contain cumulative prefixProd
+        ans[0]=1;
+        for(int i=1;i<n;i++)
+        {
+            ans[i] = ans[i-1]*nums[i-1];
+        }
+        
+        //lets multiply suffixProd now from the end
+        int suffixProd = nums[n-1];
+        for(int i=n-2;i>=0;i--)
+        {
+            ans[i] = ans[i]*suffixProd;
+            suffixProd *= nums[i];
+        }
+        
+        return ans;
+        
+    }
 };
