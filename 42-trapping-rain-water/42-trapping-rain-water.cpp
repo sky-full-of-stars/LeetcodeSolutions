52cc2e44ab26be0fc6aed2ce42986ed481e2b5fd
A sky full of stars <akshay.suryanarayan@gmail.com>
2022-09-12 18:21:00 +0530

    Time: 27 ms (47.28%), Space: 19.7 MB (96.69%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int trap(vector<int>& height) 
+    int trapBrute(vector<int>& height) 
     {
         int n = height.size();
         int ans = 0;
@@ -33,14 +33,56 @@ public:
             }
         }
         
-        return ans;
+        return ans;  
+    }
+    
+    int trap(vector<int>& height) 
+    {
+        int n = height.size();
+        int ans = 0;
+        int lo=0, hi= n-1;
+        int leftMx = 0, rightMx = 0;
         
+        while(lo<hi)
+        {
+            //[3 1 5 2 4]
+            //At 3, what amount of water is stored?
+            //water will be stored to right and left.
+            //maxSoFarInLeft will be known
+            //that is less than height[right]
+            //so min of those 2 will get trapped
+            // ie leftMax amount will be trapped on top
+            
+            //next iteration at 1,
+            // 3 will be leftMx and 4>3
+            // so (3-1) amount of water gets stored
+            if(height[lo] < height[hi]) 
+            {
+                leftMx = max(leftMx, height[lo]);
+                ans += (leftMx - height[lo]);
+                lo++;
+            }
+            //at 5,
+            //left = 5, right=4
+            // calculate amt of water gets stored on 4
+            // since 5>4 -> this is leftMax
+            // rightMax we know already
+            // since rightMax < 5(leftMax), this much of water get trapped.
+            else
+            {
+                rightMx = max(rightMx, height[hi]);
+                ans += (rightMx - height[hi]);
+                hi--;
+            }
+        }
+        
+        return ans;
     }
 };
 
 
 //.    what amount of water can be stored in idx 4? 
-//     1320134
+//        132014
 //             .
 //         .   .
 //         ..  .
