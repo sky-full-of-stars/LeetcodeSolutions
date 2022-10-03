35a8124577177b7f7d3c2c0fe9292e417a2f5da4
Akshay <akshay.suryanarayan@gmail.com>
2022-10-04 04:52:57 +0530

    Time: 27 ms (5.92%), Space: 13.7 MB (71.06%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -1,5 +1,25 @@
 class Solution 
 {
+    /*
+easy solution to think. 
+tricky implementation.
+
+A) we always want to consider positives.
+
+B) with negatives 2 cases:
+    1) even num of negatives: consider all of them
+    2) odd num of negatives : 
+        a) ignore either 1st odd num or
+        b) last odd num. 
+        consider everything in between.
+        And,
+        value for 
+            a) ans = suffix prod of all till 1st negative num
+            b) ans = prefix prod of all till last negative num
+            
+C) with 0 in between, calculate ans for all non	zero subsequences.
+*/
+    
 public:
     int maxProduct(vector<int>& nums)
     {
