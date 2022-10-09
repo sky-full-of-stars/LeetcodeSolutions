ea3666968363340137537e818c3f8669d0c25b3d
Akshay <akshay.suryanarayan@gmail.com>
2022-10-10 04:45:10 +0530

    Time: 192 ms (27.23%), Space: 48.5 MB (43.39%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,42 +1,18 @@
 class Solution
 {
 public:
-    // bool canJump(vector<int>& nums) 
-    // {
-    //     int n = nums.size();
-    //     int mxReach = 0;
-    //     for(int i=0; i<=mxReach; i++)
-    //     {
-    //         mxReach = max(mxReach, i+ nums[i]);
-    //         if(mxReach >= n-1)
-    //         {
-    //             return true;
-    //         }
-    //     }
-    //     return false;
-    // }
-    bool canJump(vector<int>& nums) 
-    {
-        int n = nums.size();
-        pair<int,int> range = {0,0};
-        int jumps = 0;
-        int mxReach = 0;
-        while(true)
+        bool canJump(vector<int>& nums) 
         {
-            jumps++;
-            
-            for(int i= range.first; i<= range.second; i++)
-                mxReach = max(mxReach, i + nums[i]);
-            
-            range = {range.second+1, mxReach};
-            
-            if(mxReach >= n-1)
-                return true;
-            
-            if(range.second < range.first)
-                return false;
+            int n = nums.size();
+            int mxReach = 0;
+            for(int i=0; i<=mxReach; i++)
+            {
+                mxReach = max(mxReach, i+ nums[i]);
+                if(mxReach >= n-1)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
-        return true;
-    }
-    
 };
