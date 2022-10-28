9029c50d637ec20359b275f54cc42070306b9660
Akshay <akshay.suryanarayan@gmail.com>
2022-10-28 14:28:02 +0530

    Time: 4 ms (94.23%), Space: 6 MB (76.72%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,8 +1,13 @@
 class Solution {
 public:
     
+    // https://ibb.co/Mc4pP8h
+    // https://ibb.co/MBV9mLh
+    // this question single handedly ruined my chance at my fav company in my fav place.
+
     string intToRoman(int num) 
     {
+        // who even can think storing 900/400/90/40/4 is intuitive
         string sym[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
         int val[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         string ans = "";
