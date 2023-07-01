aa4ce24ce949a77c764cd2ae153a406dd96a3549
Akshay <akshay.suryanarayan@gmail.com>
2023-07-02 00:07:40 +0530

    Time: 1222 ms (62.70%), Space: 6.9 MB (62.54%) - LeetHub

---
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.java
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int n,k;
+    int mxSum;
+    int ans = INT_MAX;
+    void solve(int cookieNo, vector<int>& cookies, vector<int>& sums, int unfairness)
+    {
+        if(cookieNo == n)
+        {
+            ans = min(ans, unfairness);
+            return;
+        }
+        
+        for(int kid=0; kid<(k+1/2); kid++)
+        {
+            sums[kid] += cookies[cookieNo];
+            int curUnfairness = max(unfairness, sums[kid]);
+            solve(cookieNo+1, cookies, sums, curUnfairness);
+            sums[kid] -= cookies[cookieNo];
+        }
+    }
+    int distributeCookies(vector<int>& cookies, int _k) 
+    {
+        n = cookies.size();
+        k = _k;
+        vector<int> sums(k,0);
+        
+        int unfairness = INT_MIN;
+        solve(0, cookies, sums, unfairness);
+        
+        return ans;
+    }
+};
